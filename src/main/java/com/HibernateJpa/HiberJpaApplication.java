0d98c164d9ab163b21694f1fcd2beb9d392b46a7
Arunrajput1904 <143942467+Arunrajput1904@users.noreply.github.com>
2026-03-30 20:00:48 +0530

    Add greeting message to main method

--- src/main/java/com/HibernateJpa/HiberJpaApplication.java
+++ src/main/java/com/HibernateJpa/HiberJpaApplication.java
@@ -14,6 +14,7 @@ public class HiberJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HiberJpaApplication.class, args);
+		System.out.println("Hello bhai g");
 	}
 
 
